1835308401e30d888ec27fe4bb9bbe14cc126a58
Khozem <khozemsodawala@gmail.com>
2023-03-19 19:05:46 -0400

    Add files via upload

---
+++ Project/main.js
@@ -0,0 +1,30 @@
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(500,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+} 
+
+function gotPoses(results)
+{
+    if(results.lenght>0)
+    {
+        console.log(results);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized!');
+}
+
+
+function draw()
+{
+    background('#6434eb');
+}
